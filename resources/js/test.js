ad345ed8aee76c9e187405ffd779220ede244cab
Aziz <aziz345568667@gmail.com>
2024-10-16 18:29:04 +0300

    commits

--- resources/js/test.js
+++ resources/js/test.js
@@ -1,4 +1,21 @@
-document.getElementById('toggle-button').addEventListener('click', function() {
-    var listContainer = document.getElementById('list-container');
-    listContainer.classList.toggle('visible');
-});
+function toggleDropdown(button) {
+    var dropdown = null;
+    if (button.id === "currency-button-left") {
+        dropdown = document.getElementById("currency-dropdown-left");
+    }
+    else {
+        dropdown = document.getElementById("currency-dropdown-right");
+    }
+    dropdown.style.display = dropdown.style.display === "block" ? "none" : "block";
+}
+
+function selectCurrency(currency, side) {
+    if (side === 'left') {
+        document.getElementById("currency-button-left").innerText = currency;
+        document.getElementById("currency-dropdown-left").style.display = "none";
+    }
+    else {
+        document.getElementById("currency-button-right").innerText = currency;
+        document.getElementById("currency-dropdown-right").style.display = "none";
+    }
+}
