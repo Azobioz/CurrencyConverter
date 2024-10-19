437af1ea7956a024d4101a469029bb725a522229
Aziz <aziz345568667@gmail.com>
2024-10-20 01:36:12 +0300

    commit

--- resources/js/test.js
+++ resources/js/test.js
@@ -1,4 +1,32 @@
+const url = '';
+
+
 function toggleDropdown(button) {
+    let currencyList = null
+    if (button.id === "currency-button-left") {
+        currencyList = document.getElementById('currency-dropdown-left');
+    }
+    else {
+        currencyList = document.getElementById('currency-dropdown-right');
+    }
+
+
+    fetch(url)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok ' + response.statusText);
+            }
+            return response.json();
+        })
+        .then(data => {
+            for (let currencyCode in data.conversion_rates) {
+                const currency = document.createElement("div");
+                currency.textContent = `${currencyCode}`;
+                currencyList.appendChild(currency);
+            }
+        })
+
+
     var dropdown = null;
     if (button.id === "currency-button-left") {
         dropdown = document.getElementById("currency-dropdown-left");
@@ -9,13 +37,12 @@ function toggleDropdown(button) {
     dropdown.style.display = dropdown.style.display === "block" ? "none" : "block";
 }
 
-function selectCurrency(currency, side) {
-    if (side === 'left') {
-        document.getElementById("currency-button-left").innerText = currency;
-        document.getElementById("currency-dropdown-left").style.display = "none";
-    }
-    else {
-        document.getElementById("currency-button-right").innerText = currency;
-        document.getElementById("currency-dropdown-right").style.display = "none";
-    }
+
+
+
+function selectCurrency(side) {
+
+
+
+
 }
