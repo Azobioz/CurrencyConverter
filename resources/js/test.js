9c5335defd585b26cd6c402820a28e588bc270b4
Aziz <aziz345568667@gmail.com>
2024-10-22 02:53:20 +0300

    commit

--- resources/js/test.js
+++ resources/js/test.js
@@ -10,7 +10,6 @@ function toggleDropdown(button) {
         currencyList = document.getElementById('currency-dropdown-right');
     }
 
-
     fetch(url)
         .then(response => {
             if (!response.ok) {
@@ -22,7 +21,19 @@ function toggleDropdown(button) {
             for (let currencyCode in data.conversion_rates) {
                 const currency = document.createElement("div");
                 currency.textContent = `${currencyCode}`;
+
+                currency.addEventListener('click', () => selectCurrency(button, currencyCode));
+                currency.addEventListener('click', () => {
+                    if (button.id === "currency-button-left") {
+                        setCurrencyValueAndCalculate(document.getElementById("left-value"));
+                    }
+                    else {
+                        setCurrencyValueAndCalculate(document.getElementById("right-value"));
+                    }
+                });
                 currencyList.appendChild(currency);
+
+
             }
         })
 
@@ -38,11 +49,60 @@ function toggleDropdown(button) {
 }
 
 
+function selectCurrency(button, currencyName) {
 
+    button.textContent = currencyName;
 
-function selectCurrency(side) {
+    // Скрываем выпадающий список после выбора
+    if (button.id === "currency-button-left") {
+        document.getElementById("currency-dropdown-left").style.display = "none";
+    }
+    else {
+        document.getElementById("currency-dropdown-right").style.display = "none";
+    }
 
+}
 
+function setCurrencyValueAndCalculate(input) {
 
+    if (input.id === "left-value") {
+
+        fetch(url)
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Network response was not ok ' + response.statusText);
+                }
+                return response.json();
+            })
+            .then(data => {
+                const currencyType = document.getElementById("currency-button-left").textContent;
+
+                const exchangeRate = data.conversion_rates[currencyType];
+
+                input.value = exchangeRate;
+
+                const rightInput = document.getElementById("right-value");
+                rightInput.value = exchangeRate * data.conversion_rates[document.getElementById("currency-button-right").textContent];
+            })
+    }
+    else {
+        fetch(url)
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Network response was not ok ' + response.statusText);
+                }
+                return response.json();
+            })
+            .then(data => {
+                const currencyType = document.getElementById("currency-button-right").textContent;
+
+                const exchangeRate = data.conversion_rates[currencyType];
+
+                input.value = exchangeRate
+
+                const leftInput = document.getElementById("left-value");
+                leftInput.value = exchangeRate * data.conversion_rates[document.getElementById("currency-button-left").textContent];
+            })
+    }
 
 }
